6388e7bdcfc3a2a48ec7fd69decfffac19a69d9a
Gabriel Dario <dxosen@gmail.com>
2025-01-29 03:48:23 -0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Gabriel Dario on 1/29/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
